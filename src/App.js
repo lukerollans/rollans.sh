37b653c4cfede261a774d9896ac8440b4684e0ed
Luke Rollans <hello@lukerollans.me>
2017-07-16 21:56:17 +0800

    Add the meat and potatoes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Sidebar from './components/sidebar'
+import MeatAndPotatoes from './components/meat-and-potatoes'
 
 const App = (props) => {
   return (
     <div className="site-wrapper">
+      <Sidebar />
+      <MeatAndPotatoes />
     </div>
   )
 }
