09f4603cfb3979c6ec24418e9b5234870b928c64
Luke Rollans <hello@lukerollans.me>
2019-08-04 17:58:13 +0800

    Remove erroneous line

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Sidebar from './components/sidebar';
 import MeatAndPotatoes from './components/meat-and-potatoes';
-import OpenSourceExample from './components/open-source-example';
 
 const App = props => {
   return (
